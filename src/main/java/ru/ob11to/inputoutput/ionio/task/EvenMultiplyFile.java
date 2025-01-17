9c4f35f149614a6ecc5c7f83fcd33648b6edbb6e
Artem Dagridjan <ob11to61@yandex.ru>
2025-01-17 18:09:52 +0300

    2.1. FileInputStream [#4898] + Задача

---
+++ src/main/java/ru/ob11to/inputoutput/ionio/task/EvenMultiplyFile.java
@@ -0,0 +1,25 @@
+package ru.ob11to.inputoutput.ionio.task;
+
+import java.io.FileOutputStream;
+
+public class EvenMultiplyFile {
+    public static void main(String[] args) {
+        try (var out = new FileOutputStream("data/task/even.txt")) {
+            int size = 10;
+            String newLine;
+            for (int i = 1; i <= size; i++) {
+                // Внутренний цикл для второго числа (вертикальная ось)
+                for (int j = 1; j <= size; j++) {
+                    // Формирование строки таблицы умножения: i * j = результат
+                    newLine = "%d * %d = %d".formatted(i, j, i * j);
+                    out.write(newLine.getBytes());
+                    out.write(System.lineSeparator().getBytes()); // Переход на новую строку после каждого произведения
+                }
+                out.write(System.lineSeparator().getBytes()); // Дополнительная строка для разделения строк таблицы
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
--- src/main/java/ru/ob11to/inputoutput/ionio/task/EvenNumberFile.java
+++ src/main/java/ru/ob11to/inputoutput/ionio/task/EvenNumberFile.java
@@ -1,25 +1,33 @@
 package ru.ob11to.inputoutput.ionio.task;
 
-import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Arrays;
 
 public class EvenNumberFile {
+
     public static void main(String[] args) {
-        try (var out = new FileOutputStream("data/task/even.txt")) {
-            int size = 10;
-            String newLine;
-            for (int i = 1; i <= size; i++) {
-                // Внутренний цикл для второго числа (вертикальная ось)
-                for (int j = 1; j <= size; j++) {
-                    // Формирование строки таблицы умножения: i * j = результат
-                    newLine = "%d * %d = %d".formatted(i, j, i * j);
-                    out.write(newLine.getBytes());
-                    out.write(System.lineSeparator().getBytes()); // Переход на новую строку после каждого произведения
-                }
-                out.write(System.lineSeparator().getBytes()); // Дополнительная строка для разделения строк таблицы
+        try (var in = new FileInputStream("data/even.txt")) {
+            int read;
+            StringBuilder sb = new StringBuilder();
+            while ((read = in.read()) != -1) {
+                sb.append((char) read);
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+            Integer[] array = Arrays.stream(sb.toString().split(System.lineSeparator()))
+                    .map(Integer::valueOf)
+                    .toArray(Integer[]::new);
+
+            Arrays.stream(array)
+                    .forEach(number -> {
+                        if (number % 2 == 0) {
+                            System.out.println(number + " – четное число");
+                        } else {
+                            System.out.println(number + " – нечетное число");
+                        }
+
+                    });
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
-
