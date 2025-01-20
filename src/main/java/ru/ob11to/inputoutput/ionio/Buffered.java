e104194c78c35d8529acce6670d1664296bd5c6b
Artem Dagridjan <ob11to61@yandex.ru>
2025-01-20 10:25:39 +0300

    5.0. Буферизированные потоки [#504979] - ознакомительная

---
+++ src/main/java/ru/ob11to/inputoutput/ionio/Buffered.java
@@ -0,0 +1,18 @@
+package ru.ob11to.inputoutput.ionio;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Buffered {
+    public static void main(String[] args) {
+        try (BufferedInputStream input = new BufferedInputStream(new FileInputStream("data/input.txt"));
+             BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream("data/output.txt"))) {
+            output.write(input.readAllBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
