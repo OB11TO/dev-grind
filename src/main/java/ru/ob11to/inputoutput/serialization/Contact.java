3f02586cdfb83a2c95462fa88ac4539a179f4f41
Artem Dagridjan <ob11to61@yandex.ru>
2025-01-23 14:46:14 +0300

    1. Что такое Сериализация? [#313163]

---
+++ src/main/java/ru/ob11to/inputoutput/serialization/Contact.java
@@ -0,0 +1,36 @@
+package ru.ob11to.inputoutput.serialization;
+
+import java.io.*;
+import java.nio.file.Files;
+
+public record Contact(int zipCode, String phone) implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 5780672321457505738L;
+
+    @Override
+    public String toString() {
+        return "Contact{" + "zipCode=" + zipCode + ", phone='" + phone + '\'' + '}';
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        final Contact contact = new Contact(123456, "+7 (111) 111-11-11");
+
+        /* Запись объекта во временный файл, который удалится системой */
+        File tempFile = Files.createTempFile(null, null).toFile();
+        try (FileOutputStream fos = new FileOutputStream(tempFile);
+             ObjectOutputStream oos =
+                     new ObjectOutputStream(fos)) {
+            oos.writeObject(contact);
+        }
+
+        /* Чтение объекта из файла */
+        try (FileInputStream fis = new FileInputStream(tempFile);
+             ObjectInputStream ois =
+                     new ObjectInputStream(fis)) {
+            final Contact contactFromFile = (Contact) ois.readObject();
+            System.out.println(contactFromFile);
+        }
+    }
+}
+
