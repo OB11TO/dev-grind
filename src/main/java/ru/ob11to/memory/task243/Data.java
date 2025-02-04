d7d10a684e26a04c66d7b8d0a44bbdef62cd9ca5
Artem Dagridjan <ob11to61@yandex.ru>
2025-02-04 16:30:20 +0300

    3. Эксперименты с различными GC [#1590]

---
+++ src/main/java/ru/ob11to/memory/task243/Data.java
@@ -0,0 +1,7 @@
+package ru.ob11to.memory.task243;
+
+public interface Data {
+    void insert(int elements);
+
+    int[] getClone();
+}
---
+++ src/main/java/ru/ob11to/memory/task243/Sort.java
@@ -0,0 +1,5 @@
+package ru.ob11to.memory.task243;
+
+public interface Sort {
+    boolean sort(Data data);
+}
