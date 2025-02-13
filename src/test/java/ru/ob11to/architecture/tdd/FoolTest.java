82cf87d66e1eb95508507004bc220749d9a139a9
Artem Dagridjan <ob11to61@yandex.ru>
2025-02-13 16:19:14 +0300

    2. Принципы Kiss, Dry и Yagni [#238813] add task

---
+++ src/test/java/ru/ob11to/architecture/tdd/FoolTest.java
@@ -0,0 +1,34 @@
+package ru.ob11to.architecture.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class FoolTest {
+
+    @Test
+    void whenSetupDel5ThenReturnBuzz() {
+        int startAt = 5;
+        assertThat(Fool.setup(startAt)).isEqualTo("Buzz");
+    }
+
+    @Test
+    public void whenFizzBuzz() {
+        assertThat(Fool.setup(15)).isEqualTo("FizzBuzz");
+    }
+
+    @Test
+    public void whenFizz() {
+        assertThat(Fool.setup(3)).isEqualTo("Fizz");
+    }
+
+    @Test
+    public void whenBuzz() {
+        assertThat(Fool.setup(5)).isEqualTo("Buzz");
+    }
+
+    @Test
+    public void when11() {
+        assertThat(Fool.setup(11)).isEqualTo("11");
+    }
+}
