7796243c485c2291da3cf506a00838958f0d14d1
Artem Dagridjan <ob11to61@yandex.ru>
2025-07-08 16:30:42 +0300

    Stream API - toArray(IntFunction) - Кастомная реализация IntFunction<HardArray[]>

--- src/main/java/ru/ob11to/functionstyle/functions/ArrayRunner.java
+++ src/main/java/ru/ob11to/functionstyle/functions/ArrayRunner.java
@@ -11,7 +11,40 @@ public class ArrayRunner {
         String[] array1 = list.toArray(new String[0]);
         String[] array2 = list.toArray(new ArrayIntFunction());
         System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(array1));
         System.out.println(Arrays.toString(array2));
+
+        HardArray[] array3 = list.stream()
+                .map(HardArray::new)
+                .toArray(new HardArrayIntFunction());
+        System.out.println(Arrays.toString(array3));
+    }
+
+    static class HardArray {
+
+        private final String value;
+
+        public HardArray(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return value;
+        }
+
+    }
+
+    static class HardArrayIntFunction implements IntFunction<HardArray[]> {
+
+        @Override
+        public HardArray[] apply(int value) {
+            return new HardArray[value];
+        }
     }
 
     static class ArrayIntFunction implements IntFunction<String[]> {
